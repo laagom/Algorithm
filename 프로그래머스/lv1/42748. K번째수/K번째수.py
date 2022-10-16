b825afd67de4dea4249be2ff0fc9bc5a3e574981
CjyooonG <103980058+laagom@users.noreply.github.com>
2022-10-16 10:48:36 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -0,0 +1,17 @@
+def solution(array, commands):
+    answer = []
+    
+    # 들어온 commands만큼 반복을 돌면서 
+    for i, command in enumerate(commands, start=1): 
+        # print(f'i : {i} and command : {command}')
+        # i~j까지 자르고 
+        # 나온 리스트를 정렬
+        sorted_array = sorted(array[command[0]-1:command[1]])
+        # print(f'sorted_array : {sorted_array} and command[2] : {command[2]}')
+        
+        # k번째 있는 수 구해
+        # print(sorted_array[command[2]-1])
+        answer.append(sorted_array[command[2]-1])    
+    print(answer)
+        
+    return answer
