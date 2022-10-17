b80c05ccfc43d13fd1a7b4bd781f9f2737d7534b
CjyooonG <103980058+laagom@users.noreply.github.com>
2022-10-17 10:56:22 +0900

    [level 2] Title: 가장 큰 수, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,4 +1,10 @@
 def solution(numbers):
-    numbers = list(map(str, numbers))
-    numbers.sort(key=lambda x: x * 3, reverse=True)
-    return str(int(''.join(numbers)))
+    
+    # numbers요소를 문자열로 변환
+    arry = list(map(str, numbers))
+
+    # lambda 함수로 str으로 변환된 요소를 3번씩 이어붙인 값을 역정렬
+    arry.sort(key=lambda x: x*3, reverse=True)
+        
+    # 0, 0, 0 인 경우 테스트 케이스 통과하지 못하는 경우 발생 int형으로 변경우 str으로 재 변경
+    return str(int(''.join(arry)))
