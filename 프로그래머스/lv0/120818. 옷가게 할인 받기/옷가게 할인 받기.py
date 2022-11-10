404a234cf94edae5c7862a6bfe9f86d2b9c456e8
CjyooonG <103980058+laagom@users.noreply.github.com>
2022-11-11 00:35:36 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -1,13 +1,10 @@
-def solution(price: int) -> int:
-    
-    if price >= 500000:
-        return int(price * 0.8)
-    if price >= 300000:
-        return int(price * 0.9)
-    if price >= 100000:
-        return int(price * 0.95)
-    return price
 
-if __name__ == '__main__':
-    print(solution(150000))  # 142,500
-    print(solution(580000))  # 464,000
+def solution(price):
+    
+    discout = {500000 : 0.8, 300000 : 0.9, 100000 : 0.95}
+    
+    for base, cnt in discout.items():
+        if price >= base:
+            return int(price* cnt)
+    return price
+ 
