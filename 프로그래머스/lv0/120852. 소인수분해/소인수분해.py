b71a83281449d74c3b47ed022c967834bcf65805
CjyooonG <103980058+laagom@users.noreply.github.com>
2022-11-18 00:32:20 +0900

    [level 0] Title: 소인수분해, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120852. 소인수분해/소인수분해.py
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.py
@@ -1,13 +1,11 @@
 def solution(n):
+    k = 2
     answer = []
-    q=[]
-    for i in range(2,n+1):   #n의 약수 구하기
-        if n%i ==0:
-            q.append(i)    
+    while n>1:
+        if n%k==0:
+            answer.append(k)
+            while n%k==0:
+                n//=k
+        k+=1
 
-    for i in q:  #n의 약수 중에서 소수 구하기
-        for j in range(2,5000):
-            if i*j in q:
-                q.remove(i*j)
-    q.sort()
-    return q
+    return answer
