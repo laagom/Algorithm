f4a169bcfd6fbeb8f724bde44f10be691895a3ec
CjyooonG <103980058+laagom@users.noreply.github.com>
2022-11-27 15:20:25 +0900

    [level 0] Title: 평행, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120875. 평행/평행.py
+++ 프로그래머스/lv0/120875. 평행/평행.py
@@ -1,10 +1,11 @@
 def solution(dots):
     # 두점의 기울기가 동일 한 값이 존재하면 1
     # 두점의 기울기가 동일 한 값이 없으면 0
+    # 두점의 기울기 (x1, y1) (x2, y2)
+    # 기울기 = (y2-y1)/(x2-x1)
     
     answer = 0
     leanArr = []
-    
     
     for i in range(0, len(dots)):
         for j in range(i+1, len(dots)):
