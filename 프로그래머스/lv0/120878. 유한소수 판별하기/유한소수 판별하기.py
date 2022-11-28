a902d1a964aae27c193ef255bcfbfa8bff2d1339
CjyooonG <103980058+laagom@users.noreply.github.com>
2022-11-28 13:53:50 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.py
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.py
@@ -1,10 +1,16 @@
 def solution(a, b):
-    b //= gcd(a,b)
-    while b%2==0:
-        b//=2
-    while b%5==0:
-        b//=5
-    return 1 if b==1 else 2
+    answer = 0
+    b //= gcd(a, b)
+    
+    while b%2 == 0:
+        b //= 2
+        
+    while b%5 == 0:
+        b //= 5
+    
+    
+    return 1 if b == 1 else 2
+
 
 
 # 최대공약수
