88722a6d8b24a65d142b0fff914e80433bb12fdc
CjyooonG <103980058+laagom@users.noreply.github.com>
2022-11-28 13:48:32 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.py
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.py
@@ -1,38 +1,14 @@
-from collections import defaultdict
-def make_primes(n:int)->list:
-    primes = [True] * (n + 1)
-    primes[0:2] = [False, False]
-    for num in range(2, int(n * 0.5) + 1):
-        if primes[num]:
-            for i in range(num ** 2, n + 1, num):
-                primes[i] = False
-    return primes
-
-def make_prime_factorization(n):
-    primes = make_primes(n)
-    prime_numbers = [x for x in range(2, n + 1) if primes[x]]
-    result = defaultdict(int)
-    for number in prime_numbers:
-        while n % number == 0 and n > 1:
-            result[number] += 1
-            n //= number
-        if n == 1:
-            break
-    return result
-
 def solution(a, b):
-    numerator = make_prime_factorization(a)
-    denominator = make_prime_factorization(b)
+    b //= gcd(a,b)
+    while b%2==0:
+        b//=2
+    while b%5==0:
+        b//=5
+    return 1 if b==1 else 2
 
-    # 분모 약분
-    for key, value in numerator.items():
-        if key in denominator:
-            denominator[key] = max(0, denominator[key] - value)
 
-    # 유한 소수 판별
-    for key, value in denominator.items():
-        if key == 2 or key == 5:
-            continue
-        if value > 0:
-            return 2
-    return 1
+# 최대공약수
+def gcd(a, b):
+    for i in range(min(a, b), 0, -1):
+        if a%i == 0 and b%i ==0:
+            return i
