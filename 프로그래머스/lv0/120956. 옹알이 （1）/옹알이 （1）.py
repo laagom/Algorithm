2181246bb0e1e71f41ef183c72d64b317ba16b48
CjyooonG <103980058+laagom@users.noreply.github.com>
2022-12-01 01:30:45 +0900

    [level 0] Title: 옹알이 (1), Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -0,0 +1,12 @@
+def solution(babbling):
+    answer = 0
+    can_char = ["aya", "ye", "woo", "ma"]
+
+    for i in range(len(babbling)):
+        for check in can_char:
+            
+            if check in babbling[i]:
+                print(f'check : {check}')
+                print(f'babbling[i] : {babbling[i]}')
+                babbling[i] = babbling[i].replace(check, ' ')
+    return [char.replace(' ','') for char in babbling].count('')
