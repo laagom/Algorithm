f0655fd30c84301e813949d691046d93db5f710c
CjyooonG <103980058+laagom@users.noreply.github.com>
2023-02-24 18:07:17 +0900

    [level 1] Title: 폰켓몬, Time: 1.64 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -1,14 +1,18 @@
 def solution(nums):
-    answer      = 0
     pocket_dict = dict()
-    qty         = len(nums)//2
+    qty = len(nums)//2 # 선택할 수 있는 포켓몬 종류
     
+    # 연구실 포켓몬 분류
     for num in nums:
+        # pocket_dict에 있으면 count++
         if num in pocket_dict.keys():
             pocket_dict[num] += 1
         else:
+            # pocket_dict에 없으면 count 1
             pocket_dict[num] = 1
-            
-    num_mon = len(pocket_dict.keys())
     
-    return qty if qty <= num_mon else num_mon
+    # pocket_dict의 포켓몬 종류
+    num_kind = len(pocket_dict.keys())
+    
+    # 선택할 수 있는 수가 포켓몬의 종류보다 적다면 그 수만큼만 그렇지 않으면 포켓몬 종류 수
+    return qty if qty <= num_kind else num_kind
