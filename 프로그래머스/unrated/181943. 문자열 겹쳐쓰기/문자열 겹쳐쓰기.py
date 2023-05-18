4cea0152bdaa4e6f0b63743d6f6840922243e0b9
CjyooonG <103980058+laagom@users.noreply.github.com>
2023-05-18 17:02:39 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
@@ -1,7 +1,13 @@
 def solution(my_string, overwrite_string, s):
-    my_string = list(my_string)   
-    for i in range(len(overwrite_string)):
-        for j, str in enumerate(my_string):
-            if i+s == j:
-                my_string[j] = overwrite_string[i]
-    return ''.join(my_string)
+    # 풀이 1: 2중 루프
+    # my_string = list(my_string)   
+    # for i in range(len(overwrite_string)):
+    #     for j, str in enumerate(my_string):
+    #         if i+s == j:
+    #             my_string[j] = overwrite_string[i]
+    # return ''.join(my_string)
+    
+    # 풀이 2: 슬라이싱
+    return my_string[0:s] + overwrite_string + my_string[s+len(overwrite_string):]
+
+    
