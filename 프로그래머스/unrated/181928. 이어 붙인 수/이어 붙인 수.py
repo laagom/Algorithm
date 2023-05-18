7a3a23b6ce4e64b05c20a2d1f28617e82f13eb99
CjyooonG <103980058+laagom@users.noreply.github.com>
2023-05-18 20:05:45 +0900

    [unrated] Title: 이어 붙인 수, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.py
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.py
@@ -1,4 +1,13 @@
 def solution(num_list):
-    oven = ''.join([str(num) for num in num_list if num%2 == 0])
-    odd = ''.join([str(num) for num in num_list if num%2 == 1])
-    return int(oven) + int(odd)
+    # oven = ''.join([str(num) for num in num_list if num%2 == 0])
+    # odd = ''.join([str(num) for num in num_list if num%2 == 1])
+    oven = ""
+    odd = ""
+    
+    for num in num_list:
+        if num%2 == 0:
+            oven += str(num)
+        else:
+            odd += str(num)
+    
+    return int(oven) + int(odd) 
