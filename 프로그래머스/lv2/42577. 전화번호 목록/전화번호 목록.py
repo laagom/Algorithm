2c28ceeb1d2aa7f8eeecfafaddf49c23f596bf33
CjyooonG <103980058+laagom@users.noreply.github.com>
2023-05-21 16:39:48 +0900

    [level 2] Title: 전화번호 목록, Time: 91.60 ms, Memory: 28 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,12 +1,7 @@
 def solution(phone_book):
-    # 전화번호부를 오름차순으로 정렬
     phone_book.sort()
-    
-    # loop를 돌리며 index, index+1을 비교하여 포함된 경우 False
-    for i in range(len(phone_book)-1):
-        if len(phone_book[i]) < len(phone_book[i+1]):
-            if phone_book[i+1].startswith(phone_book[i]):
-                return False
-            
-    # 위의 로직을 통과 하면 그대로 True
+   
+    for i in range(1, len(phone_book)):
+        if phone_book[i].startswith(phone_book[i-1]):
+            return False
     return True
