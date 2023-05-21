ef91443e5543d365c9bed74745c55ecdc6b02a1b
CjyooonG <103980058+laagom@users.noreply.github.com>
2023-05-21 16:12:44 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 76.62 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,14 +1,20 @@
-def solution(participant, completion):   
-    participant_count = {}
-    for person in participant:
-        if person in participant_count.keys():
-            participant_count[person] += 1
-        else:
-            participant_count[person] = 1
+# def solution(participant, completion):   
+#     participant_count = {}
+#     for person in participant:
+#         if person in participant_count.keys():
+#             participant_count[person] += 1
+#         else:
+#             participant_count[person] = 1
     
-    for person in completion:
-        participant_count[person] -= 1
+#     for person in completion:
+#         participant_count[person] -= 1
     
-    participant_count = {key:value for key, value in participant_count.items() if value != 0}
+#     participant_count = {key:value for key, value in participant_count.items() if value != 0}
 
-    return ''.join(list(participant_count.keys()))
+#     return ''.join(list(participant_count.keys()))
+
+import collections
+def solution(participant, completion):   
+    non_pass = collections.Counter(participant)-collections.Counter(completion)
+
+    return list(non_pass)[0]
