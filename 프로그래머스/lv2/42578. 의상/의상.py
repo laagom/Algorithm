5d7075b6ef03cfdc8733c141175a4e4d330abd41
CjyooonG <103980058+laagom@users.noreply.github.com>
2023-05-21 17:55:37 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.py
+++ 프로그래머스/lv2/42578. 의상/의상.py
@@ -6,7 +6,8 @@ def solution(clothes):
             clothes_dict[cloth[1]] = 1
         else:
             clothes_dict[cloth[1]] += 1
-
+    
+    # (x+a)(x+b)(x+c) = x3 + (a+b+c)x2 + (ab+bc+ca)x + (abc)의 계산식
     for h in clothes_dict.values():
         answer = (h+1)*answer
 
