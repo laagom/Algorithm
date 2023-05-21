93b2dc64c721b8e27f7d23cc31fa695371f93a7a
CjyooonG <103980058+laagom@users.noreply.github.com>
2023-05-21 16:06:18 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 47.53 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,18 +1,14 @@
-def solution(participant, completion): 
-    players = {}
-    
-    # 경기에 참가한 선수들을 dictionary에 할당
-    # 동명이 있을 수 있게 때문에 선수의 수는 2이상이 될 수 있다.
+def solution(participant, completion):   
+    participant_count = {}
     for person in participant:
-        if person in players.keys():    
-            players[person] += 1
+        if person in participant_count.keys():
+            participant_count[person] += 1
         else:
-            players[person] = 1
+            participant_count[person] = 1
     
-    # 경기에 완료한 선수들을 차감
-    # 결과적으로 1이 남는 선수가 완료하지 못한 선수가 됨
     for person in completion:
-        players[person] += -1
+        participant_count[person] -= 1
     
-    # 결과가 1이 남는 선수의 이름을 반환
-    return ''.join([person for person, count in players.items() if count == 1])
+    participant_count = {key:value for key, value in participant_count.items() if value != 0}
+
+    return ''.join(list(participant_count.keys()))
