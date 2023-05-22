9995f079e596601a4912e462d680c54fe8be1609
CjyooonG <103980058+laagom@users.noreply.github.com>
2023-05-22 17:28:47 +0900

    [unrated] Title: 0 떼기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181847. 0 떼기/0 떼기.py
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.py
@@ -1,7 +1,7 @@
 def solution(n_str):
     n_str = list(n_str)
     temp_str = list(n_str)
-    for i in range(0, len(n_str)):
+    for i in range(0, len(temp_str)):
         if temp_str[i] == "0":
             del n_str[0]
         else:
