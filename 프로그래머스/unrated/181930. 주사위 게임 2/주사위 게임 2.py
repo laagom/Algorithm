5c2846b3e75ae5729cc17b2688cc465792c0f821
CjyooonG <103980058+laagom@users.noreply.github.com>
2023-05-22 15:24:10 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.py
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.py
@@ -1,13 +1,10 @@
 def solution(a, b, c):
     point = 0
     if a != b and b != c and c != a:
-        print(1)
         point = a+b+c
     elif a == b and b == c and b == c:
-        print(3)
         point = (a+b+c)*(a**2+b**2+c**2)*(a**3+b**3+c**3)
     else:
-        print(2)
         point = (a+b+c)*(a**2+b**2+c**2)
           
     return point
