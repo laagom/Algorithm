8434a44565119498525b957f5fd02e7ef45d0415
CjyooonG <103980058+laagom@users.noreply.github.com>
2023-05-24 22:56:32 +0900

    [unrated] Title: 날짜 비교하기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.py
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.py
@@ -1,12 +1,10 @@
 import datetime
 
 def solution(date1, date2):
-    answer = 0
-
     convert_date1 = datetime.datetime(date1[0], date1[1], date1[2])
     convert_date2 = datetime.datetime(date2[0], date2[1], date2[2])
     
     if convert_date1 < convert_date2:
         return 1
     
-    return answer
+    return 0
