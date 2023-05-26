18e1bae5abfa1db3f08c0d9a4bf2d86833fb19b8
CjyooonG <103980058+laagom@users.noreply.github.com>
2023-05-26 13:13:38 +0900

    [unrated] Title: qr code, Time: 0.00 ms, Memory: 9.88 MB -BaekjoonHub

--- 프로그래머스/unrated/181903. qr code/qr code.py
+++ 프로그래머스/unrated/181903. qr code/qr code.py
@@ -1,2 +1,6 @@
 def solution(q, r, code):
-    return ''.join(char for i, char in enumerate(code) if int(i%q) == r)
+    #  내 풀이     
+    # return ''.join(char for i, char in enumerate(code) if int(i%q) == r)
+    
+    # 남 풀이
+    return code[r::q]
