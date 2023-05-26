582c638d8b9fe8caf5d515c59edb22a819f49902
CjyooonG <103980058+laagom@users.noreply.github.com>
2023-05-26 21:37:51 +0900

    [unrated] Title: 전국 대회 선발 고사, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
+++ 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
@@ -1,11 +1,16 @@
+# def solution(rank, attendance):
+#     hash_map = {}
+#     attend = []
+    
+#     for num, boolean in zip(rank, attendance):
+#         if boolean:
+#             hash_map[rank.index(num)] = num
+    
+#     sorted_hash = sorted(hash_map.items(), key=lambda x: x[1])
+    
+#     return 10000*sorted_hash[0][0] + 100*sorted_hash[1][0] + sorted_hash[2][0]
+
 def solution(rank, attendance):
-    hash_map = {}
-    attend = []
-    
-    for num, boolean in zip(rank, attendance):
-        if boolean:
-            hash_map[rank.index(num)] = num
-    
-    sorted_hash = sorted(hash_map.items(), key=lambda x: x[1])
-    
-    return 10000*sorted_hash[0][0] + 100*sorted_hash[1][0] + sorted_hash[2][0]
+    arr = sorted([(x, i)for i, x in enumerate(rank) if attendance[i]])
+
+    return 10000*arr[0][1]+100*arr[1][1]+arr[2][1]
