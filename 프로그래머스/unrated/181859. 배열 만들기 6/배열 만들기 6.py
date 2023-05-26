7a8416823243e054a3f41bc812c2e3c14d889a16
CjyooonG <103980058+laagom@users.noreply.github.com>
2023-05-26 21:01:09 +0900

    [unrated] Title: 배열 만들기 6, Time: 22.04 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.py
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.py
@@ -2,12 +2,10 @@ def solution(arr):
     stack = []   
     i = 0
     while i < len(arr):
-        if not stack:
-            stack.append(arr[i])
-        elif stack[-1] == arr[i]:
+        if stack and stack[-1] == arr[i]:
             stack.pop()
-        elif stack[-1] != arr[i]:
-            stack.append(arr[i])
+        else:
+            stack.append(arr[i]) 
         i += 1
         
     if not stack:
