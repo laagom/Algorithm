054b69b5517a5f383999b9f12aa17e177e05752e
CjyooonG <103980058+laagom@users.noreply.github.com>
2023-05-27 20:51:05 +0900

    [unrated] Title: 리스트 자르기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.py
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.py
@@ -1,23 +1,11 @@
 def solution(n, slicer, num_list):
-    answer = []
     a, b, c = slicer
     
-    
     if n == 1:
-        print(num_list[:b+1])
         return num_list[:b+1]
     if n == 2:
-        print(num_list[a:])
         return num_list[a:]
     if n == 3:
-        print(num_list[a:b+1])
         return num_list[a:b+1]
     if n == 4:
-        print(num_list[a:b+1:c])
         return num_list[a:b+1:c]
-    
-n = 4
-slicer = [1, 4, 2]
-num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-# print(solution(n, slicer, num_list))
