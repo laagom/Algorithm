76091fbcf64261eca60e064209ea919192685750
CjyooonG <103980058+laagom@users.noreply.github.com>
2023-05-28 23:41:45 +0900

    [level 1] Title: 예산, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.py
+++ 프로그래머스/lv1/12982. 예산/예산.py
@@ -1,18 +1,12 @@
-# def solution(d, budget):
-#     budget_limit = 0
-#     dept = []
-    
-#     d = sorted(d)
-#     for num in d:
-#         budget_limit += num
-#         dept.append(num)
-#         if budget_limit > budget:
-#             budget_limit -= num
-#             dept.pop()
-#     return len(dept)
-
 def solution(d, budget):
-    d.sort()
-    while budget < sum(d):
-        d.pop()
-    return len(d)
+    budget_limit = 0
+    dept = []
+    
+    d = sorted(d)
+    for num in d:
+        budget_limit += num
+        dept.append(num)
+        if budget_limit > budget:
+            budget_limit -= num
+            dept.pop()
+    return len(dept)
