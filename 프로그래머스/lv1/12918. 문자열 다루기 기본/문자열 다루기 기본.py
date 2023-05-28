f037b38042b1fac68ef6f3ee7ec7e3d05fcf2951
CjyooonG <103980058+laagom@users.noreply.github.com>
2023-05-28 15:24:38 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -1,10 +1,2 @@
 def solution(s):
-    answer = True
-    length = len(s)
-    if length in [4, 6]:
-        for char in s:
-            if char.isalpha():
-                return False
-    else:
-        return False
-    return answer
+    return len(s) in [4, 6] and s.isdigit()
