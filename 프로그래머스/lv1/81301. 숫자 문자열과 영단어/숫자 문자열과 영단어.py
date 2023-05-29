4d163d6568473264d12ff8a62d5d2534bb04d88d
CjyooonG <103980058+laagom@users.noreply.github.com>
2023-05-30 01:08:11 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,20 +1,24 @@
 def solution(s):
     number = {
-        "zero": 0, "one": 1, "two": 2,
-        "three": 3, "four": 4, "five": 5,
-        "six": 6, "seven": 7, "eight": 8,
-        "nine": 9
+        "zero": "0", "one": "1", "two": "2",
+        "three": "3", "four": "4", "five": "5",
+        "six": "6", "seven": "7", "eight": "8",
+        "nine": "9"
     }
-    string =""
-    result = []
-    for char in s:
-        if char.isalpha():
-            string += char
-        else:
-            result.append(int(char))
+    
+    for key, value in number.items():
+        s = s.replace(key, value)
+
+#     result = []
+#     for char in s:
+#         if char.isalpha():
+#             string += char
+#         else:
+#             result.append(int(char))
             
-        if string in number:
-            result.append(number[string])
-            string =""
+#         if string in number:
+#             result.append(number[string])
+#             string =""
             
-    return int(''.join(list(map(str, result))))
+#     return int(''.join(list(map(str, result))))
+    return int(s)
