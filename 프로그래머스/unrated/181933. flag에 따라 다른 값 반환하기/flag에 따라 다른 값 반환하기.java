35e82e8dd9b0494d4bf3c92699cf33757519de2d
CjyooonG <103980058+laagom@users.noreply.github.com>
2023-06-17 22:55:43 +0900

    [unrated] Title: flag에 따라 다른 값 반환하기, Time: 0.83 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181933. flag에 따라 다른 값 반환하기/flag에 따라 다른 값 반환하기.java
@@ -0,0 +1,14 @@
+import java.util.Map;
+import java.util.function.BiFunction;
+
+class Solution {
+    public int solution(int a, int b, boolean flag) {
+        String var = String.valueOf(flag);
+        Map<String, BiFunction<Integer, Integer, Integer>> calcs = Map.of(
+            "true", (num1, num2) -> num1 + num2,
+            "false", (num1, num2) -> num1 - num2
+        );
+        
+        return calcs.get(var).apply(a, b);
+    }
+}
