1af19ef65810ca950002c5869d328f00b67f628b
CjyooonG <103980058+laagom@users.noreply.github.com>
2023-06-27 09:17:45 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 3, Time: 5.79 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.java
+++ 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.java
@@ -1,15 +1,11 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int[] solution(int[] arr, int k) {
-        // 짝수 true, 홀수 false
-        boolean odd_oven = k%2 == 0 ? true : false;
-        System.out.println(odd_oven);
-        for(int i = 0; i < arr.length; i++) {
-            if(odd_oven) {
-                arr[i] += k;
-            }else{
-                arr[i] *= k;
-            }
+        if(k%2 == 0){
+            return IntStream.of(arr).map(i -> i+k).toArray();
         }
-        return arr;
+
+        return IntStream.of(arr).map(i -> i*k).toArray();
     }
 }
