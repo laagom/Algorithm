bda2964adeaf69accf001c6354c7844c22f40171
CjyooonG <103980058+laagom@users.noreply.github.com>
2023-08-14 09:57:26 +0900

    [unrated] Title: 배열 만들기 4, Time: 4.09 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.java
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.java
@@ -1,30 +1,18 @@
-import java.util.ArrayList;
+import java.util.Stack;
 
 class Solution {
-    public ArrayList<Integer> solution(int[] arr) {
-        ArrayList<Integer> stack = new ArrayList<Integer>(); 
-        int last_element;
-        int compare_element;
-        
-        // i가 0부터 arr 길이까지 반복문
+    public Stack<Integer> solution(int[] arr) {
+        Stack<Integer> stack = new Stack<Integer>();
         int i = 0;
-        while(i < arr.length) {
-            if(stack.size() == 0) {
-                stack.add(arr[i]);
+        
+        while (i < arr.length) {
+            // stack이 비어 있거나 arr[i] 값보다 작은 경우
+            if(stack.empty() || stack.peek() < arr[i]) {
+                stack.push(arr[i]);
                 i++;
-            }else{
-                // 마지막 stack 값
-                last_element = stack.get(stack.size()-1);
-                compare_element = arr[i];
-
-                // arr[i] 보다 작을 때
-                if(last_element < compare_element) {
-                    stack.add(compare_element);
-                    i++;
-                }else if(last_element >= compare_element) {
-                    // arr[i] 보다 크거나 같을 때
-                    stack.remove(stack.size()-1);
-                }
+            }else if(stack.peek() >= arr[i]){
+                // stack이 arr[i] 값보다 크거나 같은 경우
+                stack.pop();
             }
         }
         return stack;
