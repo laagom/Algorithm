a2d075a89d072c08b51c8fcd655a9bd8ab83ea1a
CjyooonG <103980058+laagom@users.noreply.github.com>
2023-08-29 10:11:20 +0900

    [unrated] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 2.57 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.java
+++ 프로그래머스/unrated/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.java
@@ -1,18 +1,29 @@
-import java.util.List;
-import java.util.ArrayList;
+//------------------------------- [내 풀이 방법] ------------------------------- //
+// import java.util.List;
+// import java.util.ArrayList;
+
+// class Solution {
+//     public String solution(String[] my_strings, int[][] parts) {       
+//         List answer = new ArrayList();
+//         int s               = 0;
+//         int e               = 0;
+           
+//         for(int i = 0; i < parts.length; i++) {
+//             s = parts[i][0];
+//             e = parts[i][1];          
+//             answer.add(my_strings[i].substring(s, e+1));
+//         }
+// 
+//         return String.join("", answer);
+//     }
+// }
+
+// ------------------------------- [Stream을 이용한 풀이 방법] ------------------------------- //
+import java.util.stream.*;
 
 class Solution {
-    public String solution(String[] my_strings, int[][] parts) {       
-        List answer = new ArrayList();
-        int s               = 0;
-        int e               = 0;
-           
-        for(int i = 0; i < parts.length; i++) {
-            s = parts[i][0];
-            e = parts[i][1];          
-            answer.add(my_strings[i].substring(s, e+1));
-        }
-
-        return String.join("", answer);
+    public String solution(String[] myStrings, int[][] parts) {
+        
+        return IntStream.range(0, myStrings.length).mapToObj(i -> myStrings[i].substring(parts[i][0], parts[i][1] + 1)).collect(Collectors.joining());
     }
 }
