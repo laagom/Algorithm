a92018c5b0040425998e062ceb4eb624b4cedc72
CjyooonG <103980058+laagom@users.noreply.github.com>
2023-08-30 14:01:02 +0900

    [unrated] Title: 접미사 배열, Time: 0.35 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181909. 접미사 배열/README.md
+++ 프로그래머스/unrated/181909. 접미사 배열/README.md
@@ -1,10 +1,10 @@
 # [unrated] 접미사 배열 - 181909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181909) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181909?language=java) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.01 ms
+메모리: 71.1 MB, 시간: 0.35 ms
 
 ### 구분
 
---
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Comparator;
+
+class Solution {
+    public List<String> solution(String my_string) {
+        List<String> list = new ArrayList<>();
+        
+        // "banana"의 b 인덱스 1부터 시작하여 a 마지막 인덱스 까지 순회
+        // 해당 인덱스를 제외한 나머지 문자열 
+        for(int i = 0; i < my_string.length(); i++) {
+            list.add(my_string.substring(i, my_string.length()));
+        }
+        list.sort(Comparator.naturalOrder());
+        return list;
+    }
+}
