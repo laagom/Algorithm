a055f1e41f1f344152c4bae36d4655364fe10607
CjyooonG <103980058+laagom@users.noreply.github.com>
2023-08-31 15:55:42 +0900

    [unrated] Title: 세로 읽기, Time: 2.50 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.java
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.java
@@ -1,15 +1,19 @@
 import java.util.*;
-
+import java.util.stream.*;
 class Solution {
     public String solution(String my_string, int m, int c) {
-        String answer = "";
-        List<String> list = new ArrayList();
+//         String answer = "";
+//         List<String> list = new ArrayList();
 
-        for(int i = 0; i < my_string.length(); i++) {
-            if(i%m == c-1) {
-                list.add(String.valueOf(my_string.charAt(i)));
-            }    
-        }        
-        return String.join("", list);
+//         for(int i = 0; i < my_string.length(); i++) {
+//             if(i%m == c-1) {
+//                 list.add(String.valueOf(my_string.charAt(i)));
+//             }    
+//         }        
+//         return String.join("", list);
+            return IntStream.range(0, my_string.length())
+            .filter(i -> i % m == c - 1)
+            .mapToObj(i -> String.valueOf(my_string.charAt(i)))
+            .collect(Collectors.joining());
     }
 }
