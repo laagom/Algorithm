881c29103b7bb4c025c10191f6636c717e84d473
CjyooonG <103980058+laagom@users.noreply.github.com>
2023-09-01 10:46:51 +0900

    [unrated] Title: 배열 만들기 1, Time: 0.02 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.java
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.java
@@ -1,17 +1,14 @@
-import java.util.List;
-import java.util.ArrayList;
-
 class Solution {
-    public List<Integer>solution(int n, int k) {
-        List<Integer> list = new ArrayList<>();
+    public int[] solution(int n, int k) {    
+        int cnt = n / k;
         int i = 1;
-        while(i <= n) {
-            // 순회로 도는 정수의 값이 k의 배수인 경우
-            if(i%k == 0) {
-                list.add(i);
-            }
+        int[] answer = new int[cnt];
+        
+        while(i <= cnt) {
+            answer[i-1] = k*i;
             i++;
         }
-        return list;
+
+        return answer;
     }
 }
