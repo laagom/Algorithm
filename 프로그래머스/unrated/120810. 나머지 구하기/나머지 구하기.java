ad29de11f4431a8a3f48273782c8b721b70823c5
CjyooonG <103980058+laagom@users.noreply.github.com>
2023-11-13 16:32:55 +0900

    [level unrated] Title: 나머지 구하기, Time: 0.02 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120810. 나머지 구하기/나머지 구하기.java
+++ 프로그래머스/unrated/120810. 나머지 구하기/나머지 구하기.java
@@ -1,5 +1,8 @@
 class Solution {
     public int solution(int num1, int num2) {
-        return num1 % num2;
+        while(num1 >= num2) {
+            num1 -= num2;
+        }
+        return num1;
     }
 }
