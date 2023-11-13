2d3fd37f7f739eb7c49323a956b070d971235957
CjyooonG <103980058+laagom@users.noreply.github.com>
2023-11-13 16:35:54 +0900

    [level unrated] Title: 나머지 구하기, Time: 0.02 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120810. 나머지 구하기/나머지 구하기.java
+++ 프로그래머스/unrated/120810. 나머지 구하기/나머지 구하기.java
@@ -1,5 +1,8 @@
 class Solution {
     public int solution(int num1, int num2) {
+        // num1을 num2로 나누어 몫이 나올때까지
+        // num1-num2
+        // 더 이상 몫이 나오지 않으면 num1을 num2로 나눈 나머지가 남음
         while(num1 >= num2) {
             num1 -= num2;
         }
