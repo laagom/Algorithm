9de4ef6350427da9d1a70d64d029c52a23a960f2
CjyooonG <103980058+laagom@users.noreply.github.com>
2024-01-06 17:45:24 +0900

    [level unrated] Title: 1로 만들기, Time: 0.02 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        for(int number : num_list) {            
+            while(number != 1) {
+                if(number%2 == 0) {
+                    number = number/2;
+                }else {
+                    number = (number-1)/2;
+                }
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
