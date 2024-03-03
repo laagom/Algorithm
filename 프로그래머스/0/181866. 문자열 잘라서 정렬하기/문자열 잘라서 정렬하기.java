8ee24780270e8350fa44a396f9777c78b07f8426
CjyooonG <103980058+laagom@users.noreply.github.com>
2024-03-03 18:05:09 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 88.60 ms, Memory: 99.9 MB -BaekjoonHub

--- 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
@@ -7,9 +7,11 @@ class Solution {
     public List<String> solution(String myString) {
         List<String> rtnList = new ArrayList<String>(Arrays.asList(myString.split("x")));
         
-        while(rtnList.contains("")) {
+        // 빈값이 들어가는 경우 제외 처리
+        while(rtnList.contains("")) 
             rtnList.remove("");
-        }
+        
+        // 정렬
         Collections.sort(rtnList);
         return rtnList;
     }
