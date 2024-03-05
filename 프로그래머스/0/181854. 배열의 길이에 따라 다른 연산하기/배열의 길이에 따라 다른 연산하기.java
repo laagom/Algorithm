aa78689fc88d60e4e9a48a73b398dc0f9defaff9
CjyooonG <103980058+laagom@users.noreply.github.com>
2024-03-06 00:41:07 +0900

    [level 0] Title: 배열의 길이에 따라 다른 연산하기, Time: 0.02 ms, Memory: 91 MB -BaekjoonHub

---
+++ 프로그래머스/0/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] solution(int[] arr, int n) {
+        int len = arr.length;
+        
+        for(int i = 0; i < arr.length; i++) {
+            // 배열의 길이가 짝수인 경우
+            if(len%2 == 0) {
+                // 홀수 위치의 값에 n 증가
+                if(i%2 == 1) {
+                    arr[i] += n;
+                }
+            // 배열의 길이가 홀수인경우
+            }else {
+                // 짝수 위치의 값에 n 증가
+                if(i%2 == 0) {
+                    arr[i] += n;
+                }
+            }
+        }
+        return arr;
+    }
+}
